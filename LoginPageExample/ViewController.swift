8492bad1be88fa918ae7260a5f8d867947f67fc0
Jon McLean <no@email.com>
2019-01-06 19:04:42 +1100

    Initial Commit

---
+++ LoginPageExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPageExample
+//
+//  Created by Jon McLean on 6/1/19.
+//  Copyright © 2019 Jon McLean Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
